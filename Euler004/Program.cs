a151553edd214af0f258ecd8a5ce4070ca4f3644
George Walker <finster@gmail.com>
2012-01-04 12:02:41 -0700

    Finished problem 4

---
+++ Euler004/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Euler004
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			int result = 0;
+			int maxi = 0;
+			int maxj = 0;
+
+			for (int i = 999; i > 0; i--)
+			{
+				for (int j = 999; j > 0; j--)
+				{
+					if (IsPalindrome(i * j) && i * j > result)
+					{
+						result = i * j;
+						maxi = i;
+						maxj = j;
+					}
+				}
+			}
+
+			Console.WriteLine(String.Format("{0} * {1} = {2}", maxi, maxj, result));
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+			Console.ReadLine();
+
+		}
+
+		public static bool IsPalindrome(int value)
+		{
+			char[] strValue = value.ToString().ToCharArray();
+			Array.Reverse(strValue);
+			if (value.ToString() == new string(strValue))
+				return true;
+			else
+				return false;
+		}
+	}
+}
