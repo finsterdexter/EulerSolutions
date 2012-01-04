0c0388a83470d2cfda952dd036bf8da745993ab7
George Walker <finster@gmail.com>
2012-01-04 10:55:00 -0700

    Finished problem 3

--- Euler003/Program.cs
+++ Euler003/Program.cs
@@ -12,7 +12,29 @@ namespace Euler003
 
 			List<long> primes = FindPrimeNumbers(600851475143);
 			List<long> output = FindPrimeFactors(600851475143, primes);
-			
+
+			foreach (long number in output)
+			{
+				Console.WriteLine(number);
+			}
+			Console.ReadLine();
+		}
+
+
+		// http://www.blackwasp.co.uk/PrimeFactors.aspx
+		private static List<long> FindPrimeFactors(long max, List<long> primes)
+		{
+			primes.Sort();
+			List<long> output = new List<long>();
+			foreach (long prime in primes)
+			{
+				if ((max % prime) == 0)
+				{
+					output.Add(prime);
+					max /= prime;
+				}
+			}
+			return output;
 		}
 
 		// http://www.blackwasp.co.uk/Eratosthenes_2.aspx
@@ -21,19 +43,31 @@ namespace Euler003
 			List<long> primes = new List<long>();
 			primes.Add(2);
 
-			for (int i = 3; i <= max; i += 2)
+			for (int i = 3; i <= Math.Sqrt(max); i += 2)
 			{
-				if (IsPrime(i))
+				if (IsPrime(i, primes))
 				{
-					primes.Add
+					primes.Add(i);
 				}
 			}
+
+			return primes;
 		}
 
-		// http://www.blackwasp.co.uk/PrimeFactors.aspx
-		private static List<long> FindPrimeFactors(long max)
+		private static bool IsPrime(long checkValue, List<long> primes)
 		{
+			bool isPrime = true;
 
+			foreach (long prime in primes)
+			{
+				if ((checkValue % prime) == 0 && prime <= Math.Sqrt(checkValue))
+				{
+					isPrime = false;
+					break;
+				}
+			}
+
+			return isPrime;
 		}
 	}
 }
