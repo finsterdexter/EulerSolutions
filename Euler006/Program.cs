3f456e088785aa462b8a258af37bd25be244dcca
George Walker <finster@gmail.com>
2012-01-04 15:42:29 -0700

    Finished problem 6

---
+++ Euler006/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Euler006
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetDifference(10));
+			Console.WriteLine(GetDifference(100));
+			Console.ReadLine();
+		}
+
+		private static long GetDifference(int max)
+		{
+			List<long> naturals = new List<long>();
+			for (int i = 1; i <= max; i++)
+			{
+				naturals.Add(i);
+			}
+
+			long squareOfTheSums = naturals.Sum() * naturals.Sum();
+			long sumOfTheSquares = naturals.Aggregate((total, next) => (next * next + total));
+
+			return squareOfTheSums - sumOfTheSquares;
+		}
+	}
+}
