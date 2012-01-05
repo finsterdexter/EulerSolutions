f3d11461813a005ddbc64d79bade6e873a70349e
George Walker <finster@gmail.com>
2012-01-05 12:22:45 -0700

    finished problem 10

---
+++ Euler010/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EulerUtils;
+using System.Diagnostics;
+
+namespace Euler010
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch timer = Stopwatch.StartNew();
+			List<long> primes = MathUtils.FindPrimeNumbers(2000000);
+
+			Console.WriteLine("Sum of first {0} primes: {1}", primes.Count, primes.Sum());
+			Console.WriteLine(timer.ElapsedMilliseconds);
+			Console.ReadLine();
+		}
+	}
+}
