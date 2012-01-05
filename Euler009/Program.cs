55ed535215e41699a70dfbed9649feb7dd3ebb26
George Walker <finster@gmail.com>
2012-01-05 11:54:25 -0700

    finished problem 8 and problem 9

---
+++ Euler009/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler009
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			for (int a = 1; a < 1000; a++)
+			{
+				for (int b = a + 1; b < 1000; b++)
+				{
+					int c = 1000 - a - b;
+					if (a * a + b * b == c * c)
+					{
+						Console.WriteLine("{0}, {1}, {2}", a, b, c);
+						Console.WriteLine(a * b * c);
+						goto Finish;
+					}
+				}
+			}
+
+		Finish:
+			Console.ReadLine();
+			
+
+		}
+	}
+}
