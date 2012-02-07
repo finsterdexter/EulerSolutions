c2b5f6731122a057c612b3aa506d8dedc6d76ddb
George Walker <finster@gmail.com>
2012-02-07 12:34:55 -0700

    finished problem 16

---
+++ Euler016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Euler016
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BigInteger number = BigInteger.Pow(2, 1000);
+			char[] digits = number.ToString().ToCharArray();
+			int total = 0;
+			foreach (char digit in digits)
+			{
+				total += Int32.Parse(Char.ToString(digit));
+			}
+			Console.WriteLine(total);
+			Console.ReadLine();
+		}
+	}
+}
